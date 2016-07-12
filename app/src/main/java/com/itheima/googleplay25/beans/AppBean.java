a3f0353be36d30e8fe4239a229a3b75022eb4355
Administrator <a4511@163.com>
2016-07-12 21:50:35 +0800

    [init]

---
+++ app/src/main/java/com/itheima/googleplay25/beans/AppBean.java
@@ -0,0 +1,13 @@
+package com.itheima.googleplay25.beans;
+
+/*
+ *  @项目名：  MyGoolePlay 
+ *  @包名：    com.itheima.googleplay25.beans
+ *  @文件名:   AppBean
+ *  @创建者:   Administrator
+ *  @创建时间:  2016/7/12 21:49
+ *  @描述：    TODO
+ */
+public class AppBean {
+    private static final String TAG = "AppBean";
+}
